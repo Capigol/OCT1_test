136574212be37de241a3e5fa77e79e4c69a7ca0a
Lucas Nicolas Alberca <86059300+Capigol@users.noreply.github.com>
2023-09-04 15:19:06 -0300

    Update OCT1_predictor_streamlit_v1.py

--- OCT1_predictor_streamlit_v1.py
+++ OCT1_predictor_streamlit_v1.py
@@ -475,9 +475,9 @@ def final_plot(final_file):
     fig = go.Figure(go.Pie(labels=keys, values=[substrates_hc, substrates_mc, non_substrates_hc, non_substrates_mc, non_conclusives]))
     
     fig.update_layout(plot_bgcolor = 'rgb(256,256,256)',  title = None, title_text=None,
-                            title_font = dict(size=25, family='Calibri', color='black'),
-                            font =dict(size=20, family='Calibri'),
-                            legend_title_font = dict(size=18, family='Calibri', color='black'),
+#                            title_font = dict(size=25, family='Calibri', color='black'),
+#                            font =dict(size=20, family='Calibri'),
+#                            legend_title_font = dict(size=18, family='Calibri', color='black'),
                             legend_font = dict(size=15, family='Calibri', color='black'))
     
     return fig
