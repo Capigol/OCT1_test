ee95a2b9f6b5375bb893f98ba32c509ed9283424
Lucas Nicolas Alberca <86059300+Capigol@users.noreply.github.com>
2023-09-04 15:43:41 -0300

    Update OCT1_predictor_streamlit_v1.py

--- OCT1_predictor_streamlit_v1.py
+++ OCT1_predictor_streamlit_v1.py
@@ -474,8 +474,7 @@ def final_plot(final_file):
     keys = ["Substrate - High confidence", "Substrate - Medium confidence", "Non Substrate - High confidence", "Non Substrate - Medium confidence", "Non conclusive"]
     fig = go.Figure(go.Pie(labels=keys, values=[substrates_hc, substrates_mc, non_substrates_hc, non_substrates_mc, non_conclusives]))
     
-    fig.update_layout(plot_bgcolor = 'rgb(256,256,256)',  title = None, title_text = None, 
-                      layout_title_text='Van Gogh: 5 Most Prominent Colors Shown Proportionally',  layout_showlegend=False,
+    fig.update_layout(plot_bgcolor = 'rgb(256,256,256)',  layout_title_text='Van Gogh',
                             title_font = dict(size=25, family='Calibri', color='black'),
                             font =dict(size=20, family='Calibri'),
                             legend_title_font = dict(size=18, family='Calibri', color='black'),
